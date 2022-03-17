2091d1cfd2a0cb6c0a92dc48051f8cfff84b4ee6
Sergey <vavilon2509@gmail.com>
2022-03-17 16:19:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bankomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ScreenMessages.Wellcome();
+
+
+        }
+    }
+}
---
+++ ScreenMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    internal class ScreenMessages
+    {
+
+        public static void Wellcome()
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"\t\tДобро пожаловать в банкомат\n\t\tСегодня {now}");
+        }
+
+        
+            
+    }
+}
